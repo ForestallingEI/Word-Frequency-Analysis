86d41d74fa73f46af38a6336e33acc59be8e575e
Toriton <toran1010suigon80@gmail.com>
2023-08-10 14:26:17 +0900

    word frequency ordered

---
+++ README.md
@@ -0,0 +1,11 @@
+Word Frequency Analysis in GO  
+===  
+
+Chat-GPT-answer based.  
+Mind Map attached in this repo.  
+
+---  
+### The text resource  
+Tarzan triumphant  
+https://www.gutenberg.org/ebooks/71372  
+[copyright status] *Public domain in the USA*
--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
+	"log"
 	"os"
+	"sort"
 	"strings"
 )
 
@@ -11,11 +14,50 @@ func main() {
 	// Read the text from a file
 	file, err := os.Open("Tarzan-triumphant.txt") // Replace with your input file
 	if err != nil {
-		fmt.Println("Error opening file:", err)
-		return
+		log.Fatal("Error opening file:", err)
 	}
 	defer file.Close()
 
+	wordFrequency := countWord(file)
+	// < Print word frequency #map# >
+	/*
+		for word, freq := range wordFrequency {
+			fmt.Printf("%s: %d\n", word, freq)
+		}
+	*/
+
+	totalCountedWords := len(wordFrequency)
+	defer fmt.Println("Total words: ", totalCountedWords)
+
+	// < Print words only. alphabetical-order #map# >
+		/*
+		sortedWords := alphabetical(wordFrequency)
+		for _,w := range sortedWords {
+			fmt.Println(w)
+		}
+		*/
+
+
+	// < Convert from map to slice, for counting-order >
+	wordcounters := converter(wordFrequency)
+	// < Print by count $slice$ >
+	
+		sort.Slice(wordcounters, func(i,j int)bool{
+			return wordcounters[i].Counter < wordcounters[j].Counter
+		})
+		fmt.Println("By count (Ascending): ", wordcounters)
+	
+	// < Print alphabetical-order $slice$ >
+	/*
+	sort.Slice(wordcounters, func(i, j int) bool {
+		return wordcounters[i].Word < wordcounters[j].Word
+	})
+	fmt.Println("By word: ", wordcounters)
+	*/
+
+}
+
+func countWord(file io.Reader) map[string]int {
 	// Create a map to store word frequency
 	wordFrequency := make(map[string]int)
 
@@ -27,20 +69,40 @@ func main() {
 
 		// Count word frequency
 		for _, word := range words {
-			// Remove punctuation and convert to lowercase for better counting
-			word = strings.TrimRight(strings.TrimLeft(word, ".,!?;:'\"()[]{}"), ".,!?;:'\"()[]{}")
+			// Remove symbols as much as possible and convert to lowercase for better counting
+			word = strings.Trim(word,".\";,?-_‘ “•'()* ")
 			word = strings.ToLower(word)
 			wordFrequency[word]++
 		}
+		if err := scanner.Err(); err != nil {
+			log.Fatal("Error reading file:", err)
+		}
 	}
+	return wordFrequency
+}
 
-	if err := scanner.Err(); err != nil {
-		fmt.Println("Error reading file:", err)
-		return
+func alphabetical(wordFrequency map[string]int) []string {
+	sortedWords := make([]string, 0, len(wordFrequency))
+	for word := range wordFrequency {
+		sortedWords = append(sortedWords, word)
 	}
+	sort.Strings(sortedWords)
+	return sortedWords
+}
 
-	// Print word frequency
-	for word, freq := range wordFrequency {
-		fmt.Printf("%s: %d\n", word, freq)
+type WordCounter struct {
+	Word    string
+	Counter int
+}
+
+type SliceCounter []WordCounter
+
+func converter(m map[string]int) SliceCounter {
+	wordcounters := make(SliceCounter, len(m))
+	i := 0
+	for word, counter := range m {
+		wordcounters[i] = WordCounter{word, counter}
+		i++
 	}
+	return wordcounters
 }
